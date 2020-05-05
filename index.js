a762535c9d8925ccc20193bdcac6620edd6098b1
Taj Loodu <tajloodu@gmail.com>
2020-05-05 15:56:14 -0400

    basic comments for clarification added

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const Discord = require('discord.js');
 const{ prefix, token} = require('./config.json');
-const client = new Discord.Client();
+const client = new Discord.Client(); //Creating new user
 
 client.once('ready', () => {
     console.log('Ready!')
-    client.user.setActivity('Gummo - 6ix9ine', {type: 'LISTENING'}).catch(console.error);
+    client.user.setActivity('Gummo - 6ix9ine', {type: 'LISTENING'}).catch(console.error); //Activity of Bot in sidebar changed to Listening to a song
 })
 
-client.on('message', message => {
+client.on('message', message => { //MESSAGE COMMANDS
 
-    if(message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])) {
+    if(message.content === "hello"){ //Basic hello greeting whenever someone says hi
+        message.reply("Greetings my love :heart_eyes: ");
+    }
+
+    if(message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])) { //Only admins and moderators have the ability to kick members
 
     
-            if(message.content.startsWith(`${prefix}kick`)){
+            if(message.content.startsWith(`${prefix}kick`)){ //bot has the ability to kick users when !kick is used
 
             let member = message.mentions.members.first();
             member.kick().then((member) => {
-                message.channel.send(":eggplant: " + member.displayName + " has been ejected!")
+                message.channel.send(":eggplant: " + member.displayName + " has been ejected!") //Kick message is displayed
             })
         }
     }
 
-    if(message.content === "hello"){
-        message.reply("Greetings my love :heart_eyes: ");
-    }
 })
 
 client.login(token);
